0fcf2308b8c6dfd7be3a9fe9273b0cdcd70f8efd
malunan <723325701@qq.com>
2023-12-07 00:14:37 +0800

    配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -118,7 +118,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/logo.png', // 导航栏logo
+    logo: '/img/logo.jpg', // 导航栏logo
     repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
@@ -172,7 +172,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
     // 博主信息 (显示在首页侧边栏)
     blogger: {
-      avatar: 'https://jsd.cdn.zzko.cn/gh/xugaoyi/image_store/blog/20200103123203.jpg',
+      avatar: '/img/logo.jpg',
       name: 'Lunan Ma',
       slogan: '努力搬砖中~',
     },
