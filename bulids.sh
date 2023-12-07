25b9e5c661421d0a5d80b9dc3c306bce8d87fbeb
malunan <723325701@qq.com>
2023-12-08 01:41:31 +0800

    1

---
+++ bulids.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# github
+git init
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/MaLunan/malunan.github.io.git HEAD:master # 发布到github
+
+# coding
+git push -f https://e.coding.net/g-galf6717/blog/malunan.coding.me.git HEAD:master # 发布到coding
+
+cd - # 退回开始所在目录
+rm -rf docs/.vuepress/dist
