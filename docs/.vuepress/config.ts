bee779bd59f337e05b74a0ee9a040bb7c76d35cc
malunan <723325701@qq.com>
2024-02-29 15:54:22 +0800

    更新文章

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -73,7 +73,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       
       {
         text: '技术',
-        link: '/technology/',
+        link: '/pages/9a7ee40fc232253e/',
         items: [
           { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
           { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
